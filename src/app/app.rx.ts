652230146303c436523cdc6323741e809d150eb8
Tracy Lee <tracy@thisdot.co>
2017-03-12 22:02:58 -0700

    something is broke im not sure what

---
+++ src/app/app.rx.ts
@@ -0,0 +1,11 @@
+export { Observable } from 'rxjs/Observable';
+export { Subject } from 'rxjs/Subject';
+export { Subscription } from 'rxjs/Subscription';
+
+import 'rxjs/add/observable/empty';
+
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
--- src/app/pun/pun.component.ts
+++ src/app/pun/pun.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Subject, Observable } from '../app.rx';
+import { PunLookup } from '../pun-lookup.service';
+
 
 @Component({
   selector: 'app-pun',
   templateUrl: './pun.component.html',
-  styles: []
+  styles: [],
+  providers: [
+    PunLookup
+  ]
 })
 export class PunComponent implements OnInit {
+  
+  suggestedKeywords: string[] = null;
 
-  constructor() { }
+  keywordsInputChange$ = new Subject<string>();
+  
+  keyword$ = this.keywordsInputChange$
+    .switchMap(text => this.puns.suggestKeywords(text))
+    .do(keywords => this.suggestedKeywords = keywords);
+    
+  constructor(
+    private puns: PunLookup
+  ) { }
 
   ngOnInit() {
   }
